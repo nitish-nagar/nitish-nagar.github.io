749ee291c68d6768b7776c84fd610a685c15f97f
Nitish <nitishnagar03@gmail.com>
2021-07-21 22:29:41 +0530

    Initial Commit Version 1.0

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+var type = new Typed('#typed',{
+    strings: ["I am a Full Stack Web Developer", "I am an Android App Developer", "I love to create"],
+    stringsElement: null,
+    typeSpeed: 25,
+    startDelay: 1000,
+    backSpeed: 25,
+    backDelay: 1000,
+    contentType: 'text',
+    onComplete: function () {
+				$("#typed").css({"background-color": "#C8412B" });
+	},
+	preStringTyped: function () { },
+	onStringTyped: function () { }
+});
